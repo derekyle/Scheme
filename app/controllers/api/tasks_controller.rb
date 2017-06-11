8e8f9aef6b7e675e59a901897f49ef0f18914bd9
Luis <luisp7@users.noreply.github.com>
2017-06-11 06:14:50 +0000

    Pusher Update

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -3,6 +3,7 @@ class Api::TasksController < ApplicationController
   Pusher.key    = ENV["pusher_key"]
   Pusher.secret = ENV["pusher_secret"]
   Pusher.logger = Rails.logger
+  Pusher.cluster = 'us2'
   Pusher.encrypted = true
 
   def index
